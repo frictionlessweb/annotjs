f4d02d276dc3bd63a6e1ab078873746994b0fe49
Joshua Hoeflich <joshuaharry411@icloud.com>
2023-03-04 17:06:49 -0600

    Switch to vite; favicon works

--- docs/pages/index.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@/components/testUtils';
-import Index from './index.page';
-
-describe("Our index page", () => {
-  test("Renders without crashing", () => {
-    render(<Index />);
-  })
-})
