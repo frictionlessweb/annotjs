ae6cb25925698ed4a0ba68549c8398f516b71f06
Joshua Hoeflich <joshuaharry411@icloud.com>
2023-03-05 11:15:11 -0600

    Change heading text

--- docs/pages/index.page.tsx
+++ docs/pages/index.page.tsx
@@ -3,7 +3,7 @@ import { Flex, Text, Heading } from "@adobe/react-spectrum";
 const Home = () => {
   return (
     <Flex direction="column">
-      <Heading>Create Next App</Heading>
+      <Heading>AnnotJS</Heading>
       <Text>This is a test!</Text>
     </Flex>
   );
