f8d940f475ec75c25814872e2370fa2d78fd3359
Joshua Hoeflich <joshuaharry411@icloud.com>
2023-03-05 13:32:19 -0600

    Progress

--- docs/components/Layout.tsx
+++ docs/components/Layout.tsx
@@ -1,4 +1,28 @@
 import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { Flex } from "@adobe/react-spectrum";
+import { Heading, Text } from "./Typography";
+
+interface NavItemProps {
+  text: string;
+  href: string;
+}
+
+const NavItem = (props: NavItemProps) => {
+  const { text, href } = props;
+  const router = useRouter();
+  return (
+    <div
+      onClick={() => {
+        router.push(href);
+      }}
+      style={{ cursor: "pointer" }}
+    >
+      <Text>{text.toUpperCase()}</Text>
+    </div>
+  );
+};
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -6,7 +30,22 @@ interface LayoutProps {
 
 const Layout = (props: LayoutProps) => {
   const { children } = props;
-  return <>{children}</>;
+  return (
+    <Flex direction="column" alignItems="center" marginX="32px">
+      <Flex justifyContent="space-between" width="300px" marginTop="16px">
+        <NavItem text="Intro" href="/" />
+        <NavItem text="Demo" href="/#demo" />
+        <NavItem
+          text="GitHub"
+          href="https://github.com/frictionlessweb/annotjs"
+        />
+        <NavItem text="API" href="/#api" />
+      </Flex>
+      <Flex marginTop="16px" direction="column" alignItems="center">
+        {children}
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Layout;
--- docs/pages/index.page.tsx
+++ docs/pages/index.page.tsx
@@ -5,21 +5,32 @@ import Link from "next/link";
 const Home = () => {
   return (
     <Flex direction="column">
-      <Heading level={1}>Annotjs</Heading>
+      <Heading id="intro" level={1}>
+        Annotjs
+      </Heading>
       <Text marginBottom="16px">
         A library for rapidly building sophisticated applications on top of PDF
         documents.
       </Text>
       <iframe
-        style={{ border: "2px solid grey" }}
+        id="demo"
+        style={{
+          border: "2px solid grey",
+          height: "400px",
+        }}
         src="https://flexlabel.org"
       ></iframe>
-      <Flex UNSAFE_style={{ maxWidth: "500px" }} direction="column">
+      <Flex UNSAFE_style={{ maxWidth: "600px" }} direction="column">
         <Text marginTop="8px" marginBottom="4px">
           <span style={{ fontWeight: "bold" }}>WARNING: </span>This library is
-          currently a work in progress. After having used it in several research
-          applications, we are currently in the process of making the technology
-          production ready
+          currently a work in progress. We have used this technology in several
+          applications and learned what sorts of API designs work well and which
+          ones do not; we are in the process of implementing the designs
+          described here, which should be suitable for production applications.
+        </Text>
+        <Text marginTop="8px">
+          If you have feedback about the ideas outlined on this web page, please
+          let us know! Feedback about these designs is very, very welcome.
         </Text>
       </Flex>
     </Flex>
