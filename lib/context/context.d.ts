8908aba3b59fcedce15d028dc5d1221a4fe3c0db
Joshua Hoeflich <joshuaharry411@icloud.com>
2023-03-07 11:06:26 -0600

    Prettify documents

--- lib/context/context.d.ts
+++ lib/context/context.d.ts
@@ -1,9 +1,13 @@
-import React from 'react';
-import type { ExtractResult } from '../api/api.js';
+import React from "react";
+import type { ExtractResult, AnnotContext } from "../api/api.js";
 
 export interface ExtractDocumentProviderProps {
   children: React.ReactNode;
   value: ExtractResult;
 }
 
-export const ExtractDocumentProvider: (props: ExtractDocumentProviderProps) => JSX.Element;
+export const ExtractDocumentProvider: (
+  props: ExtractDocumentProviderProps
+) => JSX.Element;
+
+export const useDocument: () => AnnotContext;
