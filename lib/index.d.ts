70c986588cef6061bad439a69d3f189b77e928d7
Joshua Hoeflich <joshuaharry411@icloud.com>
2023-03-08 08:47:57 -0600

    Adds exports

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,8 @@
 export * from "./math/math.js";
+/**
+ * We deliberately shouldn't expose the internal functions used in the API
+ * mapping code, as those mutate their inputs in surprising ways that
+ * consumers of our library shouldn't need to worry about.
+ */
+export { analyzeElements } from "./api/api.js";
+export * from './context/context.js';
