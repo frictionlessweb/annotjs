63a05fb5cb75727999de803c6d268d150fb5995b
Joshua Hoeflich <joshuaharry411@icloud.com>
2023-03-19 23:02:24 -0500

    Disable strict mode

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   pageExtensions: ["page.tsx"],
   transpilePackages: [
     "@adobe/react-spectrum",
