0e723ebc574ecdc3645acc5e2553c7b40651f58a
Joshua Hoeflich <joshuaharry411@icloud.com>
2023-04-16 10:43:37 -0500

    [WIP] TODO: add linters

--- voicedemo/src/AskByTyping.tsx
+++ voicedemo/src/AskByTyping.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { Flex, TextArea, Button } from "@adobe/react-spectrum";
 import Send from "@spectrum-icons/workflow/send";
+import { useDispatch } from "./providers/StateProvider";
+import { askChatGPT } from "./util/askChatGPT";
 
 export const AskByTyping = () => {
+  const [question, setQuestion] = React.useState("");
+  const dispatch = useDispatch();
+  const sendQuestion = React.useCallback(async () => {
+    dispatch({
+      type: "ADD_MESSAGE",
+      payload: { text: question, type: "user" },
+    });
+  }, [question, dispatch]);
   return (
     <Flex marginTop="32px" direction="column">
-      <TextArea />
+      <TextArea value={question} onChange={setQuestion} />
       <Flex marginY="8px" justifyContent="end">
-        <Button variant="primary">
+        <Button onPress={sendQuestion} variant="primary">
           <Send />
         </Button>
       </Flex>
