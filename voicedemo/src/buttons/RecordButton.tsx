4c512ae9364d005c4abfb54a3ef97a8e64d0aa45
Joshua Hoeflich <joshuaharry411@icloud.com>
2023-04-16 20:43:08 -0500

    Voice controls work

--- voicedemo/src/buttons/RecordButton.tsx
+++ voicedemo/src/buttons/RecordButton.tsx
@@ -1,11 +1,33 @@
 import React from "react";
 import { Button } from "@adobe/react-spectrum";
 import Voice from "@spectrum-icons/workflow/VoiceOver";
-import { RESIZE_STYLE } from '../util/constants';
+import { RESIZE_STYLE } from "../util/constants";
+import { askChatGPT } from "../util/askChatGPT";
+import { useDispatch } from "../providers/StateProvider";
+import { useVoiceControls } from "annotjs";
 
 export const RecordButton = () => {
+  const [listening, setListening] = React.useState(false);
+  const dispatch = useDispatch();
+  useVoiceControls({
+    listening,
+    setListening,
+    onMessage: async (message: string) => {
+      dispatch({
+        type: "ADD_MESSAGE",
+        payload: { type: "user", text: message },
+      });
+      const res = await askChatGPT(message);
+      dispatch({ type: "ADD_MESSAGE", payload: { text: res, type: "system" } });
+    },
+  });
   return (
-    <Button variant="primary">
+    <Button
+      onPress={() => {
+        setListening((prev) => !prev);
+      }}
+      variant={listening ? "accent" : "primary"}
+    >
       <Voice UNSAFE_style={RESIZE_STYLE} />
     </Button>
   );
