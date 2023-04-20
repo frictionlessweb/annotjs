322b29b807677559bb9ab0aba2620d541826a096
Joshua Hoeflich <joshuaharry411@icloud.com>
2023-04-20 10:14:59 -0500

    Add call to getVoices

--- voicedemo/src/main.tsx
+++ voicedemo/src/main.tsx
@@ -4,6 +4,12 @@ import { App } from "./App";
 import { Providers } from "./providers/Providers";
 import "./index.css";
 
+/**
+ * We need to call this function before rendering our app; otherwise, 
+ * text-to-speech doesn't work properly/.
+ */
+window.speechSynthesis.getVoices();
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Providers>
     <App />
