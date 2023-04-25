af0f96a0d809be86e0503fcc1b6c7831611eeafc
Joshua Hoeflich <joshuaharry411@icloud.com>
2023-04-25 17:36:12 -0500

    Try mixing up script tags

--- voicedemo/dev-dist/sw.js
+++ voicedemo/dev-dist/sw.js
@@ -67,10 +67,13 @@ if (!self.define) {
     });
   };
 }
-define(['./workbox-190d91d4'], (function (workbox) { 'use strict';
+define(['./workbox-885460c7'], (function (workbox) { 'use strict';
 
-  self.skipWaiting();
-  workbox.clientsClaim();
+  self.addEventListener('message', event => {
+    if (event.data && event.data.type === 'SKIP_WAITING') {
+      self.skipWaiting();
+    }
+  });
 
   /**
    * The precacheAndRoute() method efficiently caches and responds to
